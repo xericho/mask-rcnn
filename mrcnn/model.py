4e338e8d6bc99a0116e9cceddd3e286f895dd845
xericho <xeric.hox@gmail.com>
2018-08-13 12:01:37 -0700

    saves only best weights

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2343,7 +2343,7 @@ class MaskRCNN():
         callbacks = [
             keras.callbacks.TensorBoard(log_dir=self.log_dir,
                                         histogram_freq=0, write_graph=True, write_images=False),
-            keras.callbacks.ModelCheckpoint(self.checkpoint_path,
+            keras.callbacks.ModelCheckpoint(self.checkpoint_path, save_best_only=True,
                                             verbose=0, save_weights_only=True),
         ]
 	
